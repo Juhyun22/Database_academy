946a799e120cce617e129352e70717df72e6f67a
Juhyun-Bak <45312398+Juhyun22@users.noreply.github.com>
2021-12-16 14:36:03 +0900

    Create exo1.sql

---
+++ Day01/ex/exo1.sql
@@ -0,0 +1,22 @@
+# 사람의 정보
+-- 주석
+/*
+여러줄 주석
+*/ 
+create table `person` (
+	`id` int not null auto_increment primary key
+    , `name` varchar(10) not null 
+    , `birth` smallint not null
+    , `hobby` varchar(100) not null
+    , `introduce` text -- null을 안붙히면 기본적으로 null 값 허용
+    , `createdAt` timestamp default current_timestamp
+    , `updatedAt` timestamp default current_timestamp
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+# 스키마 정보 보기
+desc `person`;
+
+# 테이블 삭제하기
+drop table `person`;
+
+
