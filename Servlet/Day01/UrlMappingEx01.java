77b8fada1d3b7c26d8a1c60614173187d37eae8c
Juhyun-Bak <45312398+Juhyun22@users.noreply.github.com>
2021-12-28 15:33:18 +0900

    Create UrlMappingEx01.java

---
+++ Servlet/Day01/UrlMappingEx01.java
@@ -0,0 +1,20 @@
+package com.test.ex;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UrlMappingEx01 extends HttpServlet{
+	
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		PrintWriter out = response.getWriter();
+		out.println("안녕하세요.");
+		
+
+	}
+
+}
